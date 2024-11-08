37ed22c2500fcf980d8d1400940c2c74dad7480b
Grigoriy Shimichev <g.shimichev@dodobrands.io>
2024-11-08 13:44:30 +0700

    Initial Commit

---
+++ CatLepa/CatLepaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatLepaApp.swift
+//  CatLepa
+//
+//  Created by GMachine on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatLepaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatLepaTests/CatLepaTests.swift
@@ -0,0 +1,16 @@
+//
+//  CatLepaTests.swift
+//  CatLepaTests
+//
+//  Created by GMachine on 08/11/2024.
+//
+
+import Testing
+
+struct CatLepaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
